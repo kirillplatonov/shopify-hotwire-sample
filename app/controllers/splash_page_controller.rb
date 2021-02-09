8a5cb1b4aaca5b4cd3ca1c349582df7c94481495
Kirill Platonov <platonov@hey.com>
2021-02-09 11:18:01 +0300

    Check if OAuth token is valid on splash screen

--- app/controllers/splash_page_controller.rb
+++ app/controllers/splash_page_controller.rb
@@ -2,7 +2,18 @@ class SplashPageController < ApplicationController
   include ShopifyApp::EmbeddedApp
   include ShopifyApp::RequireKnownShop
 
+  # required to trigger OAuth token check
+  rescue_from ActiveResource::UnauthorizedAccess do
+    redirect_to(shop_login)
+  end
+
   def index
     @shop_origin = current_shopify_domain
+
+    # required to trigger OAuth token check
+    shop = Shop.find_by(shopify_domain: current_shopify_domain)
+    shop.with_shopify_session do
+      ShopifyAPI::Shop.current
+    end
   end
 end
