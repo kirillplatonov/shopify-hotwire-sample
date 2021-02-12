2089a6b85ac3cddc8a8d12c9be95fb1618bf5bd2
Kirill Platonov <me@kirillplatonov.com>
2021-02-12 17:23:11 +0300

    Setup URL sync with Shopify

--- app/javascript/shopify_app/index.js
+++ app/javascript/shopify_app/index.js
@@ -1,3 +1,4 @@
 require('./shopify_app')
 require('./flash_messages')
 require('./title_updater')
+require('./url_updater')
---
+++ app/javascript/shopify_app/url_updater.js
@@ -0,0 +1,25 @@
+import { History } from '@shopify/app-bridge/actions';
+
+function prepareParams(params) {
+  // Ignores Shopify params provided after OAuth since we don't
+  // need to include them in the Shopify's URL
+  const shopifyParams = ['hmac', 'locale', 'new_design_language', 'session', 'shop', 'timestamp'];
+  shopifyParams.forEach(param => params.delete(param));
+  const paramsString = params.toString();
+
+  if (paramsString.length > 0) {
+    return `?${paramsString}`;
+  } else {
+    return '';
+  }
+}
+
+// Sync URL with Shopify
+document.addEventListener("turbo:visit", (event) => {
+  const url = new URL(event.detail.url);
+  const params = prepareParams(url.searchParams);
+  const path = url.pathname + params;
+  const history = History.create(window.app);
+
+  history.dispatch(History.Action.PUSH, path);
+});
