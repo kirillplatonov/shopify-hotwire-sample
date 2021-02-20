afbf1e9b0cf085fd4159c5a53939f35273979ebc
Kirill Platonov <me@kirillplatonov.com>
2021-02-20 13:44:43 +0300

    Add current_shop helper to AuthenticatedController

--- app/controllers/authenticated_controller.rb
+++ app/controllers/authenticated_controller.rb
@@ -3,4 +3,10 @@
 class AuthenticatedController < ApplicationController
   include ShopifyApp::EnsureAuthenticatedLinks
   include ShopifyApp::Authenticated
+
+  helper_method :current_shop
+
+  def current_shop
+    @current_shop ||= Shop.find_by(shopify_domain: current_shopify_domain)
+  end
 end
