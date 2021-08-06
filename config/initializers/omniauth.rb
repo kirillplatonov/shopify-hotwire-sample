3ebcc9790f00ee7bef27aec8ab617aa83d4ddc9d
Kirill Platonov <mail@kirillplatonov.com>
2021-08-06 09:15:05 +0300

    Fix formatting

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,13 +2,13 @@
 
 Rails.application.config.middleware.use(OmniAuth::Builder) do
   provider :shopify,
-      ShopifyApp.configuration.api_key,
-      ShopifyApp.configuration.secret,
-      scope: ShopifyApp.configuration.scope,
-      setup: lambda { |env|
-        configuration = ShopifyApp::OmniAuthConfiguration.new(env['omniauth.strategy'], Rack::Request.new(env))
-        configuration.build_options
-      }
+    ShopifyApp.configuration.api_key,
+    ShopifyApp.configuration.secret,
+    scope: ShopifyApp.configuration.scope,
+    setup: lambda { |env|
+      configuration = ShopifyApp::OmniAuthConfiguration.new(env['omniauth.strategy'], Rack::Request.new(env))
+      configuration.build_options
+    }
 end
 
 if Rails.env.test?
