178f54c5c1df22192455f72424940342d4c9fbdf
FORSBERG+two <bjorn@forsbergplustwo.com>
2021-11-01 10:06:43 +0100

    Use Stimulus for handling flash messages, with turbo_streams support

---
+++ app/javascript/controllers/flash_controller.js
@@ -0,0 +1,19 @@
+import { Controller } from "stimulus";
+import { flashNotice, flashError } from "../shopify_app/flash_messages";
+
+export default class extends Controller {
+  connect() {
+    if (!window.app) {
+      return;
+    }
+    const flashData = JSON.parse(this.element.dataset.flash);
+
+    if (flashData.notice) {
+      flashNotice(flashData.notice);
+    }
+
+    if (flashData.error) {
+      flashError(flashData.error);
+    }
+  }
+}
