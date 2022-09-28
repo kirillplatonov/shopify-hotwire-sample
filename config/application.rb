8d8bb88e152c8d667325a5401804306f7c72dbfe
Kirill Platonov <kirill@platonov.net>
2022-09-28 12:18:31 +0300

    Remove Rails 6.1 defaults from config

--- config/application.rb
+++ config/application.rb
@@ -9,9 +9,6 @@ Bundler.require(*Rails.groups)
 
 module ShopifyHotwireSample
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
-
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
