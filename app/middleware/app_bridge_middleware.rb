cd817065f814d428f09f43cccc61f900f9276417
Kirill Platonov <kirill@platonov.net>
2023-01-16 18:13:48 +0300

    Fix indentation in app bridge middleware

--- app/middleware/app_bridge_middleware.rb
+++ app/middleware/app_bridge_middleware.rb
@@ -9,10 +9,10 @@ class AppBridgeMiddleware
     if request.params.has_key?("shop") && !request.params.has_key?("host")
       shop = request.params["shop"]
       if request.url.include?("admin.shopify.com")
-      shop = shop.gsub(".myshopify.com", "").gsub(".myshopify.io", "")
-      host = Base64.urlsafe_encode64("admin.shopify.com/store/#{shop}", padding: false)
+        shop = shop.gsub(".myshopify.com", "").gsub(".myshopify.io", "")
+        host = Base64.urlsafe_encode64("admin.shopify.com/store/#{shop}", padding: false)
       else
-      host = Base64.urlsafe_encode64("#{shop}/admin", padding: false)
+        host = Base64.urlsafe_encode64("#{shop}/admin", padding: false)
       end
       request.update_param("host", host)
     end
