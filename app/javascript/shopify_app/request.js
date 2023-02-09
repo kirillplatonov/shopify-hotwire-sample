b7f5e90e42d4bd5f7fb5f862fcb2ba5dd184be87
Kirill Platonov <kirill@platonov.net>
2023-02-09 23:11:40 +0400

    Remove unnecessary @shopify/app-bridge-utils package
    Closes #69

--- app/javascript/shopify_app/request.js
+++ app/javascript/shopify_app/request.js
@@ -1,5 +1,5 @@
 import { RequestInterceptor } from '@rails/request.js'
-import { getSessionToken } from "@shopify/app-bridge-utils"
+import { getSessionToken } from "@shopify/app-bridge/utilities"
 
 RequestInterceptor.register(async (request) => {
   const token = await getSessionToken(window.app)
