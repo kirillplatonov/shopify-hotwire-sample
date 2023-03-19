c52713e95ae514c9439df0fdd2613da0aae00771
Kirill Platonov <kirill@platonov.net>
2023-03-19 18:55:58 +0400

    Remove API calls from splash page

--- app/controllers/splash_page_controller.rb
+++ app/controllers/splash_page_controller.rb
@@ -3,22 +3,7 @@ class SplashPageController < ApplicationController
   include ShopifyApp::EnsureInstalled
   include ShopifyApp::ShopAccessScopesVerification
 
-  # required to trigger OAuth token check
-  rescue_from ShopifyAPI::Errors::HttpResponseError do |exception|
-    if exception.code == 401
-      redirect_to(shop_login)
-    else
-      raise exception
-    end
-  end
-
   def index
     @shop_origin = current_shopify_domain
-
-    # required to trigger OAuth token check
-    shop = Shop.find_by(shopify_domain: current_shopify_domain)
-    shop.with_shopify_session do
-      ShopifyAPI::Shop.all.first
-    end
   end
 end
